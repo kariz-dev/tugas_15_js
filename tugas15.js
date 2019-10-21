82fb4e36c11bac0cd90915af243635dd00e2e0d9
Muhammad Rizwan <muhammad.rizwan76146@gmail.com>
2019-10-22 00:02:17 +0700

    tugas 15 Muhammad Rizwan

---
+++ tugas15.js
@@ -0,0 +1,20 @@
+/* tugas 15 javascript */
+// Muhammad Rizwan
+
+
+function tugasLimabelas(){
+
+    var ab = 8;
+    var bc = 6;
+
+	console.log("nilai AB: "+ab);
+	console.log("nilai BC: "+bc);
+
+	x = Math.pow(ab,2);
+	y = Math.pow(bc,2);
+
+	console.log('panjang sisi AC pada segitiga siku-siku tersebut adalah: ',Math.sqrt(x+y));
+}
+
+
+tugasLimabelas();
